862cce279207dc982dc08f62c3509121abe669a3
Karol <karl.michalik@gmail.com>
2021-05-08 22:41:26 +0200

    PathProvider is a utility class. It is supplementing string value[path] for other classes

--- src/PathProviderClass.groovy
+++ src/PathProviderClass.groovy
@@ -1,3 +1,34 @@
-class PathProviderClass {
-    
+import com.eviware.soapui.support.GroovyUtils
+class PathProvider {
+    def log
+    def context
+    def testRunner
+    def cur_time
+    def alert = com.eviware.soapui.support.UISupport
+    String outputFilePath
+
+    PathProvider(logIn, contextIn, testRunnerIn){
+        this.log = logIn
+        this.context = contextIn
+        this.testRunner = testRunnerIn
+        cur_time = new Date().format("yyy-MM-dd HH")
+    }
+    def GetPath(){
+        def projectPath = new GroovyUtils(context).projectPath
+        def testProject = testRunner.testCase.testSuite.project
+        outputFilePath = testProject.getPropertyValue("outputPath")
+
+        if (outputFilePath == null || outputFilePath == "" || outputFilePath == " "){
+            def promptPath = alert.prompt("Output path for your files", "please provide path")
+            if (promptPath != null || promptPath != "" || promptPath != " "){
+                outputFilePath = promptPath.toString()
+                testProject.getPropertyValue("outputPath", promptPath.toString())
+            }else {
+                tp.setPropertyValue("outputPath", projectPath)
+                outputFilePath = projectPath
+            }
+
+        }
+    }
 }
+context.setProperty("pathProvider", new PathProvider(log, context,testRunner))
